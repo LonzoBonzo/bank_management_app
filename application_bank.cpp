03de75a4d9b88df943b450d278a834d44c1bba73
Lonzo <150627476+LonzoBonzo@users.noreply.github.com>
2025-10-15 22:50:55 +0000

    The second half of the Application file.

--- application_bank.cpp
+++ application_bank.cpp
@@ -40,3 +40,66 @@ void Bank::load_clients_info(string file_name) {
     fin.close();
 }
 
+Client* Bank::get_clients_info() {
+    return clients;
+}
+
+//This is the deposity btw.
+
+double Bank::deposit(double account_number, double amount) {
+    for (int i = 0; i < num_clients; i++) {
+        if (clients[i].bank_account_number == account_number) {
+            clients[i].balance += amount;
+            return clients[i].balance;
+        }
+    }
+    cout << "Account number not found. \n" << endl;
+    return -1;
+}
+
+string Bank::withdraw(double account_number, double amount) {
+    for (int i = 0; i < num_clients; i++) {
+        if (clients[i],bank_account_number == account_number) {
+            if (clients[i].balance >= amount) {
+                clients [i].balance -= amount;
+                return "Withdrawal Updated. \n";
+            } else {
+                return "Not enough balance to withdraw. \n";
+            }
+        }
+    }
+    return "Account number not found. \n";
+}
+
+void Bank::saving_info(string file_name) {
+    ofstream fout(file_name);
+    fout << num_client << endl;
+    for (int = 0; i < num_client; i++) {
+        fout << clients[i].client_name << " " << clients[i].ssn
+    }
+    fout.close();
+}
+
+void Bank::find_client(double account_number) {
+    for (int i = 0; i < num_client; i++) {
+        if (clients[i].bank_account_number == account_number) {
+            cout << "Client's Name: " << clients[i].client_name << endl;
+            cout << "SSN: " << clients[i].ssn << endl;
+            cout << "Account Number: " << clients[i].bank_account_number << endl;
+            cout << "Balance: $" << fixed << setprecision(2) << clients[i].balance << endl;
+            return;
+        }
+    }
+    cout << "This person doesn't exist. \n" << endl;
+}
+
+void Bank::add_new_client(Client new_client) {
+    Client* temp = new Client[num_client + 1];
+    for (int i = 0; i < num_client; i++) {
+        temp[i] = clients[i];
+    }
+    temp[num_client] = new_client;
+    delete[] clients;
+    clients = temp;
+    num_client++;
+}
