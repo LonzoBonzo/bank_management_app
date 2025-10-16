21b8164e47151cc1820df8482496af76696c733e
Lonzo <150627476+LonzoBonzo@users.noreply.github.com>
2025-10-16 01:07:25 +0000

    updated main to see results

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
     Bank myBank ("clients.txt");
 
-    myBank.find_client(34227); 
+    myBank.find_client(1001); 
     myBank.deposit(1001, 500);
     myBank.withdraw(1002, 100);
 
-    Client newClient = {"Charlie", "589-340-6628", 1003, 750};
+    Client newClient = {"Charlie", "589-340-6628", 1004, 750};
     myBank.add_new_client(newClient);
     myBank.saving_info("clients.txt");
 
